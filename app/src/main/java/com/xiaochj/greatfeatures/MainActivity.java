56e3fac65f6772d5ad9aca20af4c562793f91424
Xiaochj <505180646@qq.com>
2017-01-06 17:41:55 +0800

    搭起框架

--- app/src/main/java/com/xiaochj/greatfeatures/MainActivity.java
+++ app/src/main/java/com/xiaochj/greatfeatures/MainActivity.java
@@ -1,13 +1,26 @@
 package com.xiaochj.greatfeatures;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String[] mTitles = getResources().getStringArray(R.array.main_title);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,mTitles);
+        setListAdapter(arrayAdapter);
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        Toast.makeText(this,position+"",Toast.LENGTH_LONG).show();
     }
 }
---
+++ app/src/main/java/com/xiaochj/greatfeatures/network/RetrofitTest.java
@@ -0,0 +1,9 @@
+package com.xiaochj.greatfeatures.network;
+
+/**
+ * Retrofit
+ * Created by xiaochj on 17/1/6.
+ */
+
+public class RetrofitTest {
+}
