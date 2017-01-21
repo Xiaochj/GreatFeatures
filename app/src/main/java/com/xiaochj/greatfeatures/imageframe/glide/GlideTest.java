4438a13e86867e95fa58454ba3d57f3053edf593
Xiaochj <505180646@qq.com>
2017-01-21 22:21:49 +0800

    universal image loader

--- app/src/main/java/com/xiaochj/greatfeatures/imageframe/glide/GlideTest.java
+++ app/src/main/java/com/xiaochj/greatfeatures/imageframe/glide/GlideTest.java
@@ -7,6 +7,7 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 
 /**
  * Glide
@@ -25,8 +26,20 @@ public class GlideTest extends Activity {
         mLinearLayout = new LinearLayout(this);
         mLinearLayout.setLayoutParams(new FrameLayout.LayoutParams(-1,-1));
         mImageView = new ImageView(this);
-        mImageView.setLayoutParams(new LinearLayout.LayoutParams(-2,-2));
-        Glide.with(this).load(url).thumbnail(0.1f).into(mImageView);
+        mImageView.setLayoutParams(new LinearLayout.LayoutParams(300,150));
+        //.with(。。。) 可以是context,activity,fragment
+        //.placeholder(。。。) 默认的图片
+        //.error(。。。) 加载失败显示的图片
+        //.centerCrop() 图片裁剪 .fitCenter() 图片适中
+        //.skipMemoryCache(。。。) 图片的缓存策略
+        //.diskCacheStrategy(。。。) 硬盘的缓存策略
+            // DiskCacheStrategy.NONE 什么都不缓存
+            //DiskCacheStrategy.SOURCE 仅仅只缓存原来的全分辨率的图像
+            //DiskCacheStrategy.RESULT 仅仅缓存最终的图像，即降低分辨率后的（或者是转换后的）
+            //DiskCacheStrategy.ALL 缓存所有版本的图像（默认行为）
+        Glide.with(this).load(url).placeholder(android.R.drawable.ic_menu_gallery)
+                .error(android.R.drawable.stat_notify_error).skipMemoryCache(true)
+                .diskCacheStrategy(DiskCacheStrategy.NONE).fitCenter().into(mImageView);
         mLinearLayout.addView(mImageView);
         this.setContentView(mLinearLayout);
     }
--- app/src/main/java/com/xiaochj/greatfeatures/imageframe/uil/UilTest.java
+++ app/src/main/java/com/xiaochj/greatfeatures/imageframe/uil/UilTest.java
@@ -1,10 +1,62 @@
 package com.xiaochj.greatfeatures.imageframe.uil;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.FailReason;
+import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
 
 /**
+ * universal imageloader
  * Created by xiaochj on 17/1/20.
  */
 
 public class UilTest extends Activity {
+
+    static final String url = "http://news.cctvnews.cn/resource/live/english/cctv-news.png";
+    LinearLayout mLinearLayout;
+    ImageView mImageView;
+    ImageLoader imageLoader;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mLinearLayout = new LinearLayout(this);
+        mLinearLayout.setLayoutParams(new FrameLayout.LayoutParams(-1,-1));
+        mImageView = new ImageView(this);
+        mImageView.setLayoutParams(new LinearLayout.LayoutParams(300,150));
+        ImageLoaderConfiguration imageLoaderConfiguration = new ImageLoaderConfiguration.Builder(this).build();
+        imageLoader = ImageLoader.getInstance();
+        imageLoader.init(imageLoaderConfiguration);
+        imageLoader.displayImage(url, mImageView, new ImageLoadingListener() {
+            @Override
+            public void onLoadingStarted(String imageUri, View view) {
+
+            }
+
+            @Override
+            public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
+                mImageView.setBackgroundResource(android.R.drawable.stat_notify_error);
+            }
+
+            @Override
+            public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+
+            }
+
+            @Override
+            public void onLoadingCancelled(String imageUri, View view) {
+
+            }
+        });
+        mLinearLayout.addView(mImageView);
+        this.setContentView(mLinearLayout);
+    }
 }
